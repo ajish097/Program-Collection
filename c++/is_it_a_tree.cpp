dccc23d5bc4f159a837a092f339e56448df43c30
ajish097 <ajish097@gmail.com>
2017-05-18 14:43:51 +0530

    is it a tree solution

---
+++ c++/is_it_a_tree.cpp
@@ -0,0 +1,45 @@
+/*Solution for PT07Y - Is it a tree .*/
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 20010
+#define NODES 10010
+
+int graph[MAX][2],arr[10010];
+
+int find_set(int val)
+{
+    if(arr[val]==-1)
+        return val;
+    return find_set(arr[val]);
+}
+
+int main()
+{
+    int N,M,x,y;
+    bool flag = true;
+    scanf("%d%d",&N,&M);
+    for(int i=0;i<M;i++)
+        scanf("%d%d",&graph[i][0],&graph[i][1]);
+
+    memset(arr,-1,sizeof(int)*NODES);
+
+    for(int i=0;i<M;i++)
+    {
+        x=find_set(graph[i][0]);
+        y=find_set(graph[i][1]);
+
+        if(x==y)
+        {
+            flag=false;
+            break;
+        }
+        arr[x]=y;
+    }
+
+    if(flag)
+        cout<<"YES";
+    else
+        cout<<"NO";
+    return 0;
+
+}
