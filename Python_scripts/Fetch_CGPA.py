67d155467096b6eb0a05cea56e4412e8d109e490
ajish097 <ajish097@gmail.com>
2017-05-27 19:34:49 +0530

    cpga-fetcher-script

---
+++ Python_scripts/Fetch_CGPA.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+def set_url(usn):
+    if(usn >= 100):
+        return 'http://result.vtu.ac.in/cbcs_results2017.aspx?usn=1cr15cs'+str(usn)+'&sem=3'
+    elif(usn<10):
+        return 'http://result.vtu.ac.in/cbcs_results2017.aspx?usn=1cr15cs00'+str(usn)+'&sem=3'
+    else:
+        return 'http://result.vtu.ac.in/cbcs_results2017.aspx?usn=1cr15cs0'+str(usn)+'&sem=3'
+
+fw = open('stock_data.txt','w')
+for usn in range(1,197):
+    url = set_url(usn)  
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text)
+    for rows in soup.find_all('span',id = 'lblSGPA'):
+        rows = rows.string
+        if (usn<10):
+            fw.write('1cr15cs00'+str(usn)+"\t"+str(rows))
+        elif(usn >= 100):
+            fw.write('1cr15cs'+str(usn)+"\t"+str(rows))
+        else:
+            fw.write('1cr15cs0'+str(usn)+"\t"+str(rows))
+        fw.write('\n')
+
+fw.close()
