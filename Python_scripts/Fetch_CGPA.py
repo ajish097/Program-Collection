7d2b1a51f44df0216d06df0264793b0d53efa585
ajish097 <ajish097@gmail.com>
2017-05-28 13:32:40 +0530

    updated

--- Python_scripts/Fetch_CGPA.py
+++ Python_scripts/Fetch_CGPA.py
@@ -1,8 +1,10 @@
+# A python script to fetch the cgpa of all the 3rd sem cse students of cmrit. 
 import requests
 from bs4 import BeautifulSoup
 def set_url(usn):
     if(usn >= 100):
         return 'http://result.vtu.ac.in/cbcs_results2017.aspx?usn=1cr15cs'+str(usn)+'&sem=3'
+    elif(usn < 10):
         return 'http://result.vtu.ac.in/cbcs_results2017.aspx?usn=1cr15cs00'+str(usn)+'&sem=3'
     else:
         return 'http://result.vtu.ac.in/cbcs_results2017.aspx?usn=1cr15cs0'+str(usn)+'&sem=3'
@@ -15,6 +17,7 @@ for usn in range(1,197):
     soup = BeautifulSoup(plain_text)
     for rows in soup.find_all('span',id = 'lblSGPA'):
         rows = rows.string
+        if (usn < 10):
             fw.write('1cr15cs00'+str(usn)+"\t"+str(rows))
         elif(usn >= 100):
             fw.write('1cr15cs'+str(usn)+"\t"+str(rows))
---
+++ c++/Dfs-Implementation.cpp
@@ -0,0 +1,46 @@
+// This is an implementation of dfs using adjacency list.
+// For further info please refer to this link https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/tutorial/ 
+#include <bits/stdc++.h>
+using namespace std;
+int visited[1000]= {0};
+std::vector<int> v[1000];
+void dfs(int x) {
+	visited[x] = 1;
+	cout<<x<<" ";
+	for (int i = 0; i < v[x].size(); ++i) {
+		if (visited[v[x][i]] == 0) {
+			dfs(v[x][i]);                 
+		}
+	}
+
+}
+int main(){
+	int N,E;
+	int x,y;
+	cin>>N>>E;
+    for (int i = 0; i < E; ++i) {
+    	cin>>x>>y;
+    	v[x].push_back(y);
+    	v[y].push_back(x);
+    }
+    //You can remove the comment to specify the starting node
+    /*cout<<"Enter the node you wish to start with"<<endl;
+    int head;
+    cin>>head;*/
+    dfs(x);    // dfs(head); 
+return 0;
+}
+
+/*
+INPUT
+7
+6
+1 2
+2 3
+3 5
+3 6
+2 4
+4 7
+OUTPUT
+4 2 1 3 5 6 7
+*/
