14db957a5a2b0b3d20dc53dfa1fedf4607121925
ajish097 <ajish097@gmail.com>
2017-06-07 19:04:46 +0530

    updated

--- c++/Longest-path-in-a-tree.cpp
+++ c++/Longest-path-in-a-tree.cpp
@@ -5,42 +5,42 @@ using namespace std;
 bool visited[10005]; 
 std::vector<int> v[10005];
 int newHead;
-int bfs(int x){
-  int level[10005];
-  std::queue <int> Q;
-  int i,longestPath;
-  level[x] = 0;
-  visited[x] = true;     
-  Q.push(x);
-  while(!Q.empty()) {
-    newHead = i = Q.front();
-    Q.pop();
-    for (std::vector<int>::iterator itr = v[i].begin(); itr != v[i].end(); ++itr) {
-      if(!visited[*itr]) {
-        Q.push(*itr);
-        visited[*itr] = true;
-        longestPath = level[*itr] = level[i]+1;
-      }
+int bfs(int x) {
+    int level[10005];
+    std::queue <int> Q;
+    int i,longestPath;
+    level[x] = 0;
+    visited[x] = true;     
+    Q.push(x);
+    while(!Q.empty()) {
+          newHead = i = Q.front();
+          Q.pop();
+          for (std::vector<int>::iterator itr = v[i].begin(); itr != v[i].end(); ++itr) {
+              if(!visited[*itr]) {
+                Q.push(*itr);
+                visited[*itr] = true;
+                longestPath = level[*itr] = level[i]+1;
+              }
+          }
     }
-  }
   return longestPath;
 }
 int main(){
-  std::fill(visited,visited+10005,false);
-  int n;
-  int v1,v2;
-  cin>>n;
-  if(n == 0)
-    cout<<0;
-  for (int i = 0; i < n-1; ++i) {
-    cin>>v1;
-    cin>>v2;
-    v[v1].push_back(v2);
-    v[v2].push_back(v1);
-  }
-  bfs(v1);
-  std::fill(visited,visited+10005,false);
-  cout<<bfs(newHead);
+    std::fill(visited,visited+10005,false);
+    int n;
+    int v1,v2;
+    cin>>n;
+    if(n == 0)
+      cout<<0;
+    for (int i = 0; i < n-1; ++i) {
+        cin>>v1;
+        cin>>v2;
+        v[v1].push_back(v2);
+        v[v2].push_back(v1);
+    }
+    bfs(v1);
+    std::fill(visited,visited+10005,false);
+    cout<<bfs(newHead);
 return 0;
 }
 
